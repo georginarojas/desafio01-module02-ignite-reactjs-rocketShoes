f289396a119e80031752fb89950a704e510859f7
Georgina Rojas <ginarojas29@gmail.com>
2021-03-20 11:56:50 -0300

    Was implemented the cartItemsAmount function that showing the number of items added.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -27,9 +27,11 @@ const Home = (): JSX.Element => {
 
   console.log("CART ", cart);
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+    const cartItemsAmount = cart.reduce((sumAmount, product) => {
+      let key = product.id;
+      sumAmount[key] = product.amount;
+      return sumAmount;
+    }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -58,7 +60,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+               {cartItemsAmount[product.id] || 0} 
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>

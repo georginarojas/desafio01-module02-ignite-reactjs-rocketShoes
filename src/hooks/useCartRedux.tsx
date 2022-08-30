de48b6528e0bd35e9bfc7a10ed4d59f6d6c2cbc2
georginarojas <ginarojas29@gmail.com>
2022-08-30 11:53:47 -0300

    update product amount

--- src/hooks/useCartRedux.tsx
+++ src/hooks/useCartRedux.tsx
@@ -1,31 +1,37 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../state-management/store";
 import { Product, Stock } from "../types";
 import { toast } from "react-toastify";
 import { api } from "../services/api";
 import { getCart, setCart } from "../state-management/cart/cartSlice";
-import { useEffect } from "react";
+
+type UpdateProductAmount = {
+  productId: number;
+  amount: number;
+  stockSending?: Stock;
+};
 
 export const useCartRedux = () => {
   const cart = useSelector((state: RootState) => state.cart.data);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getCart())
-  }, [dispatch])
-  
+    dispatch(getCart());
+  }, [dispatch]);
+
   // API Request
   // -- Get products by ID
-  async function getProductById(id: number) {
+  const getProductById = async (id: number): Promise<Product> => {
     const response = await api.get(`/products/${id}`);
     return response.data;
-  }
+  };
 
   // -- Get stock by ID
-  async function getStockById(id: number) {
+  const getStockById = async (id: number): Promise<Stock> => {
     const response = await api.get(`/stock/${id}`);
     return response.data;
-  }
+  };
   //------------
 
   // --- Adding product to cart
@@ -52,17 +58,55 @@ export const useCartRedux = () => {
         dispatch(setCart(newCart));
         return;
       } else {
-        // updateProductAmount({
-        //   productId: productId,
-        //   amount: cartProduct.amount + 1,
-        // });
+        updateProductAmount({
+          productId: productId,
+          amount: cartProduct.amount + 1,
+          stockSending: stock,
+        });
       }
     } catch {
       toast.error("Erro na adição do produto");
     }
   };
+
+  // -- Updating list products of cart
+  const updateProductAmount = async ({
+    productId,
+    amount,
+    stockSending,
+  }: UpdateProductAmount) => {
+    try {
+      const stock = stockSending ? stockSending : await getStockById(productId);
+
+      if (!stock) {
+        console.error("no stock");
+        throw new Error();
+      }
+
+      if (amount > stock.amount) {
+        toast.error("Quantidade solicitada fora de estoque");
+        return;
+      }
+
+      if (amount < 1) {
+        console.error("no amount");
+        throw new Error();
+      }
+
+      if (amount <= stock.amount) {
+        const cartIndex = cart.findIndex((cart) => cart.id === productId);
+        const cartIdNewAmount = { ...cart[cartIndex], amount };
+        let newCart = [...cart];
+        newCart.splice(cartIndex, 1, cartIdNewAmount);
+        dispatch(setCart(newCart));
+      }
+    } catch {
+      toast.error("Erro na alteração de quantidade do produto");
+    }
+  };
   return {
     cart,
-    addProduct
+    addProduct,
+    updateProductAmount,
   };
 };

16f27714832107e831554a848b3f4a395e2c7cb3
georginarojas <ginarojas29@gmail.com>
2022-08-31 16:35:26 -0300

    add delay

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,17 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: "http://localhost:3333",
+});
+
+function sleep(ms = 2000): Promise<void> {
+  console.log("Kindly remember to remove `sleep`");
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+api.interceptors.response.use(async (response) => {
+  if (process.env.NODE_ENV === "development") {
+    await sleep();
+  }
+  return response;
 });

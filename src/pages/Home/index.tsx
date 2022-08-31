ef325ea7b153fcf6320c3d61e77280ff52a5b220
georginarojas <ginarojas29@gmail.com>
2022-08-31 16:49:54 -0300

    use fetchProducts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,16 @@
 import { useEffect } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 
-import { ProductList } from "./styles";
-import { api } from "../../services/api";
+import { Loading, ProductList } from "./styles";
+// import { api } from "../../services/api";
 import { formatPrice } from "../../util/format";
 import { useCartRedux } from "../../hooks";
 
 import { useDispatch, useSelector } from "react-redux";
-import {setProducts} from '../../state-management/products/productSlice'
+// import { setProducts } from "../../state-management/products/productSlice";
 import { RootState } from "../../state-management/store";
 import { Product } from "../../types";
+import { fetchProducts } from "../../state-management/products/fetchProducts";
 
 // interface Product {
 //   id: number;
@@ -29,28 +30,32 @@ interface CartItemsAmount {
 const Home = (): JSX.Element => {
   // const [products, setProducts] = useState<ProductFormatted[]>([]);
   // const { addProduct, cart } = useCart();
-  const { cart , addProduct } = useCartRedux()
-  const cartList = cart as Product[]
-  const dispatch = useDispatch()
-  const products = useSelector((state: RootState) => state.products.list) as Product[];
+  const { cart, addProduct } = useCartRedux();
+  const cartList = cart as Product[];
+  const dispatch = useDispatch<any>();
+  const { list, isLoading } = useSelector((state: RootState) => state.products);
+  const products = list;
 
-    const cartItemsAmount = cartList.reduce((sumAmount, product) => {
+  const cartItemsAmount = cartList.reduce((sumAmount, product) => {
     // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-      let key = product.id;
-      sumAmount[key] = product.amount;
-      return sumAmount;
-    }, {} as CartItemsAmount)
+    let key = product.id;
+    sumAmount[key] = product.amount;
+    return sumAmount;
+  }, {} as CartItemsAmount);
 
   useEffect(() => {
-    async function loadProducts() {
-      const response = await api.get("/products");
-      // setProducts([...response.data]);
+    // async function loadProducts() {
+    //   const response = await api.get("/products");
+    //   // setProducts([...response.data]);
 
-      // Using redux
-      dispatch(setProducts(response.data))
-    }
+    //   // Using redux
+    //   dispatch(setProducts(response.data));
+    // }
 
-    loadProducts();
+    // loadProducts();
+
+    // Using redux thunk
+    dispatch(fetchProducts());
   }, [dispatch]);
 
   function handleAddProduct(id: number) {
@@ -58,27 +63,31 @@ const Home = (): JSX.Element => {
   }
 
   return (
-    <ProductList>
-      {products.map((product) => (
-        <li key={product.id}>
-          <img src={product.image} alt={product.title} />
-          <strong>{product.title}</strong>
-          <span>{formatPrice(product.price)}</span>
-          <button
-            type="button"
-            data-testid="add-product-button"
-            onClick={() => handleAddProduct(product.id)}
-          >
-            <div data-testid="cart-product-quantity">
-              <MdAddShoppingCart size={16} color="#FFF" />
-               {cartItemsAmount[product.id] || 0} 
-            </div>
+    <>
+      {isLoading && <Loading>Carregando...</Loading>}
+      <ProductList>
+        {!isLoading &&
+          products.map((product) => (
+            <li key={product.id}>
+              <img src={product.image} alt={product.title} />
+              <strong>{product.title}</strong>
+              <span>{formatPrice(product.price)}</span>
+              <button
+                type="button"
+                data-testid="add-product-button"
+                onClick={() => handleAddProduct(product.id)}
+              >
+                <div data-testid="cart-product-quantity">
+                  <MdAddShoppingCart size={16} color="#FFF" />
+                  {cartItemsAmount[product.id] || 0}
+                </div>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-      ))}
-    </ProductList>
+                <span>ADICIONAR AO CARRINHO</span>
+              </button>
+            </li>
+          ))}
+      </ProductList>
+    </>
   );
 };
 

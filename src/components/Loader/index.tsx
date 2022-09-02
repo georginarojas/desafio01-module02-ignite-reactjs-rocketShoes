7deb68b0bf3df79c601b1e894c8b56de5823f35d
georginarojas <ginarojas29@gmail.com>
2022-09-02 16:06:26 -0300

    create loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Spinner } from "./styles";
+
+const Loader: React.FC = () => {
+  return (
+    <Spinner>
+      <div/>
+      <div/>
+      <div/>
+    </Spinner>
+  );
+};
+
+export default Loader;
